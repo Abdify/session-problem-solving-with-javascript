9d5524719e5a4b47b07ff12df82b39c0bc520328
Abdify <abdullah.fahimm08@gmail.com>
2022-02-03 10:35:23 +0600

    updated variable assign

--- 1. Variable assign.js
+++ 1. Variable assign.js
@@ -7,3 +7,20 @@ first = first + 5;
 first *= 5;
 
 console.log(first)
+
+
+
+let arr = [3, 6, 2, 56, 32, 5, 89, 32];
+let largest = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+
+  if (element > largest) {
+    // what this line means?
+    largest = element;
+  }
+
+}
+
+console.log(largest);
